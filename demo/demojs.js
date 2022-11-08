b965164d78ed395f39bde5eda302a0a89d0c46ff
Kodiexp <angelnabro@gmail.com>
2022-11-08 23:44:04 +0100

    📁add demo files

---
+++ demo/demojs.js
@@ -0,0 +1,18 @@
+const hello = 'I am demo file';
+
+doesNotExist.nope(`demo test`);
+
+class widget extends React.Component { }
+
+const de = function () { };
+
+const demo = 100;
+
+function demo() {
+
+}
+
+const demo = () => { };
+
+console.log(joe, demo);
+console.log(demo.demo);
