2bf6c55b1203159835af452f5e75e4c1be4fcd1e
Kodiexp <angelnabro@gmail.com>
2022-11-09 15:28:36 +0100

    change demo files

---
+++ demo/demojsx.jsx
@@ -0,0 +1,13 @@
+import Head from "next/js"
+
+export default function Demofile() {
+  return (
+    <>
+      <Head>
+        <title>demo file</title>
+      </Head>
+      <Load description='Welcome to the demo file' goto='/system'/>
+    </>
+     //esto es un comentario
+  )
+}
